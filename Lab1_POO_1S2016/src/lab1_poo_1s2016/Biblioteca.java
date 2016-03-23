b58eb6aa7e0ad040baff8d0512e81f7f12a726b0
KeilorMoreira <kmoralvarado@gmail.com>
2016-03-22 20:40:59 -0600

    Agregadas Clases 5 principales
    Estudiantes, Libro, Biblioteca, Préstamo y Capitulo

---
+++ Lab1_POO_1S2016/src/lab1_poo_1s2016/Biblioteca.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1_poo_1s2016;
+
+/**
+ *
+ * @author keilor
+ */
+public class Biblioteca {
+    
+}
