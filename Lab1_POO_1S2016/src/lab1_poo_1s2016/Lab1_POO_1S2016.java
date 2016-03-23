d0d60af35e53902dd59ce9712978217cf784eb8d
KeilorMoreira <kmoralvarado@gmail.com>
2016-03-22 20:31:11 -0600

    Carpeta del proyecto NetBeans agregada

---
+++ Lab1_POO_1S2016/src/lab1_poo_1s2016/Lab1_POO_1S2016.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1_poo_1s2016;
+
+/**
+ *
+ * @author keilor
+ */
+public class Lab1_POO_1S2016 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
